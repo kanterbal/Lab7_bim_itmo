fb072eb54635fea5919a0daa8ff59378b038add7
Sergei Kanter <sdkanter@outlook.com>
2021-10-12 14:25:35 +0200

    Ex 2 done.

---
+++ Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба - ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            AreaCube(a);
+            VolumeCube(a);
+            Console.ReadKey();
+        }
+        static void AreaCube(double a)
+        {
+            Console.WriteLine("Площадь поверхности куба = {0}",6 * Math.Pow(a, 2));
+        }
+        static void VolumeCube(double a)
+        {
+            Console.WriteLine("Объем куба = {0}",Math.Pow(a, 3));
+        }
+    }
+}
